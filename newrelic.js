aa38eb6010fff0fdc06e83f8704fe5aa3ee2266f
Bob Evans <robert.evans25@gmail.com>
2026-06-09 12:42:39 -0400

    chore: added sample app to test on vercel

---
+++ newrelic.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+exports.config = {
+  opentelemetry: {
+    enabled: true
+  },
+  instrumentation: {
+    http: {
+      enabled: false
+    },
+    next: {
+      enabled: false
+    },
+    // if you're doing native `fetch` calls
+    // you must disable undici instrumentation
+    // as Next.js has wrapped `fetch` and create client spans
+    // if you do not disable undici instrumentation you will have
+    // duplicate client spans in your traces.
+    undici: {
+      enabled: false
+    },
+  },
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'trace'
+  }
+}
